aade02a9eba804a30c60bb3178b108a227f5e655
sevensora <139036375+sevensora@users.noreply.github.com>
2023-07-30 12:08:55 +0000

    Sun, Jul 30, 2023, 10:08 PM +10:00

--- population/population.c
+++ population/population.c
@@ -3,9 +3,9 @@
 
 int main(void)
 {
-    // TODO: Prompt for start size
+    string answer = get string("What's the start size of the population?")// TODO: Prompt for start size
 
-    // TODO: Prompt for end size
+    string answer = get string("What's the end size of the population?")// TODO: Prompt for end size
 
     // TODO: Calculate number of years until we reach threshold
 
