34424e39476d639a3f888449927d7994557767bf
sevensora <139036375+sevensora@users.noreply.github.com>
2023-07-30 12:32:07 +0000

    Sun, Jul 30, 2023, 10:32 PM +10:00

--- population/population.c
+++ population/population.c
@@ -3,16 +3,17 @@
 
 int main(void)
 {
-    int 
+    int startvalue, endvalue, numbyear = 0;
+    
     do
     {
         = get_int("What's the start size of the population? ");
     }
     while (n < 9);
-}
-    int y = get_int("What's the end size of the population?")// TODO: Prompt for end size
 
-    n = n + (n / 3) - (n / 4);// TODO: Calculate number of years until we reach threshold
+    int y = get_int("What's the end size of the population?")
+
+    n = n + (n / 3) - (n / 4);
 
     printf("Years: %i\n", n);
 }
