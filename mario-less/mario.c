305aee58a175839c1e855568435284672fc0d5e4
sevensora <139036375+sevensora@users.noreply.github.com>
2023-08-01 06:48:59 +0000

    Tue, Aug 1, 2023, 4:48 PM +10:00

--- mario-less/mario.c
+++ mario-less/mario.c
@@ -1,7 +1,14 @@
 #include <cs50.h>
 #include <stdio.h>
 
-int main(void) 
+int main(void)
 {
-    
+    int x;
+    do
+    {
+        x = get_int ("Please enter height: ");
+    }
+    while (height < 1 || height > 8);
+
+    for 
 }
