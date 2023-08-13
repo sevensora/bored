393b4c5ccad736f2adba083f32b1b281951be152
sevensora <139036375+sevensora@users.noreply.github.com>
2023-08-13 04:47:46 +0000

    Sun, Aug 13, 2023, 2:47 PM +10:00

--- hi.c
+++ hi.c
@@ -2,9 +2,6 @@
 
 int main(void)
 {
-    char c1 = 'H';
-    char c2 = 'I';
-    char c3 = '!';
-
-    printf("%i %i %i\n", c1, c2, c3);
+    string s = "HI!";
+    printf("%i %i %i\n", s[0], s[1], s[2]);
 }
