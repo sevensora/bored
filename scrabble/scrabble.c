0f39a0a3d2817ebc67317445e25c449510696c6f
sevensora <139036375+sevensora@users.noreply.github.com>
2023-10-15 07:48:32 +0000

    Sun, Oct 15, 2023, 6:48 PM +11:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -18,7 +18,15 @@ int main(void)
     int score1 = compute_score(word1);
     int score2 = compute_score(word2);
 
-    // TODO: Print the winner
+    if(score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+
+    else if(score2 > score1)
+    {
+        printf
+    }
 }
 
 int compute_score(string word)
