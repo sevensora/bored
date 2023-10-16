19a1fd833221d4ce64b00d6c9aa3093a43f8bbda
sevensora <139036375+sevensora@users.noreply.github.com>
2023-10-16 06:30:31 +0000

    Mon, Oct 16, 2023, 5:30 PM +11:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -15,8 +15,53 @@ int main(int argc, string argv[])
         if (!isalpha(key[i]))
         {
             printf("Usage: ./substitution\n");
-            
+            return 1;
         }
     }
+    if(strlen(key) != 26)
+    {
+        printf("Key must contain 26 characters.\n")
+        return 1;
+    }
+
+    for (int i = 0; i < strlen(key); i++)
+    {
+        for (int j = i + 1; j < strlen(key); j++)
+        {
+            if (toupper(key[i]) == toupper(key[j]))
+            {
+                printf("Usage: ./substitution key\n");
+                return 1;
+            }
+        }
+    }
+
+    string plaintext = get_string("plaintext: ");
+
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if(islower(key[i]))
+        {
+            key[i] = key[i] - 32;
+        }
+    }
+
+    printf("ciphertext: ");
+
+    for (int i = 0; i < strlen(plaintext); i++)
+    {
+        if (isupper(planintext[i]))
+        {
+            int letter = plaintext[i] - 65;
+            printf("%c", key[letter]);
+        }
+        else if(islower(plaintext[i]))
+        {
+            int letter = plaintext[i] - 97;
+            print("%c", key[letter] + 32);
+        }
+        else printf("%c", plaintext[i]);
+    }
+    printf("\n");
 
 }
