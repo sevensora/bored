733a4e26315d1d2a4cee2b0bbb09b7424c5a6a01
sevensora <139036375+sevensora@users.noreply.github.com>
2023-10-16 06:18:11 +0000

    Mon, Oct 16, 2023, 5:18 PM +11:00

--- substitution/substitution.c
+++ substitution/substitution.c
@@ -5,7 +5,18 @@ int main(int argc, string argv[])
 {
     if (argc != 2)
     {
-        printf("U)
+        printf("Usage: ./substitution key\n");
+        return 1;
+    }
+
+    string key = argv[1];
+    for (int i = 0; i < strlen(key); i++)
+    {
+        if (!isalpha(key[i]))
+        {
+            printf("Usage: ./substitution\n");
+            
+        }
     }
 
 }
