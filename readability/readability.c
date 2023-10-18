f82131704e47ce5e5a8cbf901d0bc0802422b7fe
sevensora <139036375+sevensora@users.noreply.github.com>
2023-10-18 10:49:33 +0000

    Wed, Oct 18, 2023, 9:49 PM +11:00

--- readability/readability.c
+++ readability/readability.c
@@ -31,7 +31,7 @@ int main(void)
         sentences++;
     }
 
-    float calculation = (0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 25.8;
+    float calculation = (0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 15.8;
 
     int index = round(calculation);
 
