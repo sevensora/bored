fff07d9a1263d4487ad02632fe1869d1791fcd66
sevensora <139036375+sevensora@users.noreply.github.com>
2023-10-18 10:56:02 +0000

    Wed, Oct 18, 2023, 9:56 PM +11:00

--- readability/readability.c
+++ readability/readability.c
@@ -1,41 +1,38 @@
 #include <cs50.h>
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 #include <string.h>
-
 
 int main(void)
 {
     string text = get_string("Text: ");
 
     int letters = 0;
-    for(int i = 0; i < strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
-        if((text[i] >= 'a' && text[i] <= 'z') ||
-            (text[i] >= 'A' && text[i] <= 'Z'))
-        letters++;
+        if ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z'))
+            letters++;
     }
 
     int words = 1;
-    for(int i = 0; i <strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
-        if(text[i] == ' ')
-        words++;
+        if (text[i] == ' ')
+            words++;
     }
 
     int sentences = 0;
-    for(int i = 0; i < strlen(text); i++)
+    for (int i = 0; i < strlen(text); i++)
     {
-        if(text[i] == '.' || text[i] == '!' ||
-            text[i] == '?')
-        sentences++;
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
+            sentences++;
     }
 
     float calculation = (0.0588 * letters / words * 100) - (0.296 * sentences / words * 100) - 15.8;
 
     int index = round(calculation);
 
-    if(index < 1)
+    if (index < 1)
     {
         printf("Before Grade 1\n");
         return 0;
