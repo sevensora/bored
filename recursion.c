aa0ba8911bfb681693e5689c03260b65b27314bf
sevensora <139036375+sevensora@users.noreply.github.com>
2023-11-09 05:36:53 +0000

    Thu, Nov 9, 2023, 4:36 PM +11:00

--- recursion.c
+++ recursion.c
@@ -5,16 +5,23 @@ void draw(int n);
 
 int main(void)
 {
-    draw(1);
+    int height = get_int("Height: ");
+
+    draw(height);
 }
 
 void draw(int n)
 {
+    if (n <=0)
+    {
+        return;
+    }
+
+    draw(n - 1);
+
     for (int i = 0; i < n; i++)
     {
         printf("#");
     }
     printf("\n");
-
-    draw(n + 1);
 }
