f90bafa27217ff171ec5b18f8827f840080e1b59
sevensora <139036375+sevensora@users.noreply.github.com>
2023-11-09 03:03:01 +0000

    Thu, Nov 9, 2023, 2:03 PM +11:00

--- phonebook.c
+++ phonebook.c
@@ -5,5 +5,18 @@
 int main(void)
 {
     string names[] = {"Carter", "David"};
-    
+    string numbers[] = {"+1-617-495-1000", "+1-949-468-2750"};
+
+    string name = get_string("Name: ");
+    for (int i = 0; i < 2; i++)
+    {
+        if (strcmp(names[i], name) == 0)
+        {
+            printf("Found %s\n", numbers[i]);
+            return 0;
+        }
+    }
+    printf("Not found\n");
+    return 1;
+
 }
