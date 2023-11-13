5c7a4358ef7d79931b7be133e197e31108c0d90c
sevensora <139036375+sevensora@users.noreply.github.com>
2023-11-13 01:52:29 +0000

    Mon, Nov 13, 2023, 12:52 PM +11:00

--- plurality/plurality.c
+++ plurality/plurality.c
@@ -85,7 +85,7 @@ void print_winner(void)
 
     for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes > maximum_votes)
+        if (candidates[i].votes > maximum_vote)
         {
             maximum_vote = candidates[i].votes;
         }
