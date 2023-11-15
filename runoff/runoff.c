cee192365cdc89f8013658d3ef42e9026e083472
sevensora <139036375+sevensora@users.noreply.github.com>
2023-11-15 04:13:44 +0000

    Wed, Nov 15, 2023, 3:13 PM +11:00

--- runoff/runoff.c
+++ runoff/runoff.c
@@ -142,6 +142,11 @@ bool vote(int voter, int rank, string name)
 void tabulate(void)
 {
     // TODO
+    for(int i = 0; i < voter_count; i++)
+    {
+        for(int j = 0; j < candidate_count; i++)
+        
+    }
     return;
 }
 
