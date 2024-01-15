a31b3afef6f1d885b6d0cc2c3bc04dc0d7018fbd
sevensora <139036375+sevensora@users.noreply.github.com>
2024-01-15 11:17:19 +0000

    Mon, Jan 15, 2024, 10:17 PM +11:00

--- swap.c
+++ swap.c
@@ -8,13 +8,13 @@ int main(void)
     int y = 2;
 
     printf("x is %i, y is %i\n", x, y);
-    swap(x, y);
+    swap(&x, &y);
     printf("x is %i, y is %i\n", x, y);
 }
 
-void swap(int a, int b)
+void swap(int *a, int *b)
 {
-    int tmp = a;
-    a = b;
-    b = tmp;
+    int tmp = *a;
+    a = *b;
+    b = *tmp;
 }
