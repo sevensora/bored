1c1ed6feb97b67a08cd5d6e261a9b375f24b4561
sevensora <139036375+sevensora@users.noreply.github.com>
2024-01-15 12:50:26 +0000

    Mon, Jan 15, 2024, 11:50 PM +11:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -22,7 +22,36 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
 // Convert image to sepia
 void sepia(int height, int width, RGBTRIPLE image[height][width])
 {
-    
+    for(int i = 0; i < height; i++)
+    {
+        for(int j = 0; j < width; j++)
+        {
+            float originalRed = image[i][j].rgbtRed;
+            float originalGreen = image[i][j].rgbtGreen;
+            float originalBlue = image[i][j].rgbtBlue;
+
+            int sepiaRed = round(.393 * originalRed + .769 * originalGreen + .189 * originalBlue);
+            int sepiaGreen = round(.349 * originalRed + .686 * originalGreen + .168 * originalBlue);
+            int sepiaBlue = round(.272 * originalRed + .534 * originalGreen + .131 * originalBlue);
+
+            if(sepiaRed > 255)
+            {
+                sepiaRed = 255;
+            }
+            if(sepiaGreen > 255)
+            {
+                sepiaGreen = 255;
+            }
+            if(sepiaBlue > 255)
+            {
+                sepiaBlue = 255;
+            }
+
+            image[i][j].rgbtRed = sepiaRed;
+            image[i][j].rgbtGreen = sepiaGreen;
+            image[i][j].rgbtBlue = sepiaBlue;
+        }
+    }
     return;
 }
 
