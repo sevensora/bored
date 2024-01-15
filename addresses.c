b17a1fa9a1d2061b8f1482ff1271b5fb96bd7ec7
sevensora <139036375+sevensora@users.noreply.github.com>
2024-01-15 10:52:42 +0000

    Mon, Jan 15, 2024, 9:52 PM +11:00

--- addresses.c
+++ addresses.c
@@ -3,11 +3,9 @@
 
 int main(void)
 {
-    string s = "HI!";
-    printf("%p\n", s);
-    printf("%p\n", &s[0]);
-    printf("%p\n", &s[1]);
-    printf("%p\n", &s[2]);
-    printf("%p\n", &s[3]);
+    char *s = "HI!";
+    printf("%c\n", *s);
+    printf("%c\n", *(s + 1));
+    printf("%c\n", *(s + 2));
 
 }
