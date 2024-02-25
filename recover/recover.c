11a5dc4b56e72c545c0c61631e1604bd1aed38bc
sevensora <139036375+sevensora@users.noreply.github.com>
2024-02-25 12:02:23 +0000

    Sun, Feb 25, 2024, 11:02 PM +11:00

--- recover/recover.c
+++ recover/recover.c
@@ -1,16 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
+typedef uint8_t BTYE;
 
 int main(int argc, char *argv[])
 {
     if (argc != 2)
     {
         printf("Usage: ./recover IMAGE\n");
-        return 1:
+        return 1;
     }
 
-    FILE 
+    FILE *input_file = fopen(argv[1], "r");
+
+    if (input_file == NULL)
+    {
+        printf("Could not open file");
+        return 2;
+    }
+
+    unsigned char buffer[512];
+
+    int count_image = 0;
+
+    FILE *output_file = NULL;
+
+    char *filename = malloc(8 * sizeof(char));
+
+    while (fread(buffer, sizeof(char), 512, input_file))
+    {
+        if(buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0)
+        {
+            sprintf(filename, "%03i.jpg", count_image);
+
+            count_image++;
+        }
+        if (output_file !=NULL)
+        {
+            fwrite(buffer, sizeof(char), 512, output_file);
+        }
+    }
+    free(filename);
+    fclose(output_file);
+    fclose(input_file);
 
 
-
+    return 0;
 }
