bcf58487d605557798ce91319b6118016b3cf91d
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-09 03:09:41 +0000

    Sat, Mar 9, 2024, 2:09 PM +11:00

--- list.c
+++ list.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    int *list = malloc(3 * sizeof(int))
+    int *list = malloc(3 * sizeof(int));
     if (list == NULL)
     {
         return 1;
@@ -13,10 +13,29 @@ int main(void)
     list[1] = 2;
     list[2] = 3;
 
-    int *
+    int *tmp = malloc(4 * sizeof(int));
+    if (tmp == NULL)
+    {
+        free(list);
+        return 1;
+    }
 
     for (int i = 0; i < 3; i++)
     {
+        tmp[i] = list[i];
+    }
+
+    tmp[3] = 4;
+
+    free(list);
+
+    list = tmp;
+
+    for (int i = 0; i < 4; i++)
+    {
         printf("%i\n", list[i]);
     }
+
+    free(list);
+    return 0;
 }
