35a1cea1a85bb811464da244ab27fafdd56e00f3
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-10 09:17:50 +0000

    Sun, Mar 10, 2024, 8:17 PM +11:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -53,7 +53,7 @@ person *create_family(int generations)
         new_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0]
+        new_person->alleles[0] = parent0->alleles[rand() % ]
     }
 
     // If there are no generations left to create
