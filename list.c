fc3170bec48500d796bd6f00abc162c7d99feee4
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-10 08:30:43 +0000

    Sun, Mar 10, 2024, 7:30 PM +11:00

--- list.c
+++ list.c
@@ -15,44 +15,30 @@ int main(int argc, char *argv[])
 
     for (int i = 1; i < argc; i++)
     {
-        
+        int number = atoi(argv[i]);
+        if (n == NULL)
+        {
+            return 1;
+        }
+        n->number = number;
+        n->next = NULL;
+
+        n->next = list;
+        list = n;
     }
 
-
-
-    int *list = malloc(3 * sizeof(int));
-    if (list == NULL)
+    node *ptr = list;
+    while (ptr != NULL)
     {
-        return 1;
+        printf("%i\n", ptr->number);
+        ptr = ptr->next;
     }
 
-    list[0] = 1;
-    list[1] = 2;
-    list[2] = 3;
-
-    int *tmp = malloc(4 * sizeof(int));
-    if (tmp == NULL)
+    ptr = list;
+    while (ptr != NULL)
     {
-        free(list);
-        return 1;
+        node *next = ptr->next;
+        free(ptr);
+        ptr = next;
     }
-
-    for (int i = 0; i < 3; i++)
-    {
-        tmp[i] = list[i];
-    }
-
-    tmp[3] = 4;
-
-    free(list);
-
-    list = tmp;
-
-    for (int i = 0; i < 4; i++)
-    {
-        printf("%i\n", list[i]);
-    }
-
-    free(list);
-    return 0;
 }
