801d12f5c13ca0bc09692ddfbe3f4b6a0d487b71
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-10 09:23:16 +0000

    Sun, Mar 10, 2024, 8:23 PM +11:00

--- inheritance/inheritance.c
+++ inheritance/inheritance.c
@@ -49,29 +49,35 @@ person *create_family(int generations)
         person *parent1 = create_family(generations - 1);
 
         // TODO: Set parent pointers for current person
-        new_person->parents[0] = parent0;
-        new_person->parents[1] = parent1;
+        n_person->parents[0] = parent0;
+        n_person->parents[1] = parent1;
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
-        new_person->alleles[0] = parent0->alleles[rand() % ]
+        n_person->alleles[0] = parent0->alleles[rand() % 2];
+        n_person->alleles[1] = parent1->alleles[rand() % 2];
     }
 
     // If there are no generations left to create
     else
     {
         // TODO: Set parent pointers to NULL
+        n_person->parents[0] = NULL;
+        n_person->parents[1] = NULL;
 
         // TODO: Randomly assign alleles
+        n_person->alleles[0] = random_allele();
+        n_person->alleles[1] = random_allele();
     }
 
     // TODO: Return newly created person
-    return NULL;
+    return n_person;
 }
 
 // Free `p` and all ancestors of `p`.
 void free_family(person *p)
 {
     // TODO: Handle base case
+    
 
     // TODO: Free parents recursively
 
