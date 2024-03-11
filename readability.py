d9e88f84bb6b5b7f055237589f881ca888062bd7
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-11 11:40:43 +0000

    Mon, Mar 11, 2024, 10:40 PM +11:00

--- readability.py
+++ readability.py
@@ -7,9 +7,9 @@ sentence = letter = word = 0
 for x in text:
     if x.isalpha():
         letters += 1
-    elif x = "":
+    elif x == "":
         word += 1
-    elif x == "." or x == "?" or i == "!"
+    elif x == "." or x == "?" or i == "!":
         sentence += 1
 
 index = .0588 * (letter/word * 100) - .296 * (sentence/word * 100) - 15.8
@@ -17,5 +17,7 @@ index = .0588 * (letter/word * 100) - .296 * (sentence/word * 100) - 15.8
 if index < 1:
     print("Before Grade 1")
 elif index >= 16:
-    print("Grade 16)
+    print("Grade 16 and above")
+else:
+    print("Grade ", round(index))
 
