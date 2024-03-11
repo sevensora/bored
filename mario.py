af369a694f94ea9a26a839c9741011cf3bbfba4e
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-11 02:42:13 +0000

    Mon, Mar 11, 2024, 1:42 PM +11:00

--- mario.py
+++ mario.py
@@ -1,9 +1,4 @@
-from cs50 import get_int
-
-while True:
-    n = get_int("Height: ")
-    if n > 0:
-        break
-
-for i in range(n):
-    print("#")
+for i in range(3):
+    for j in range(3):
+        print("#", end="")
+    print()
