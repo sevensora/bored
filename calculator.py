86f35d79f48bfdfb6c8d1e4d316b151c7c29eae9
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-11 02:38:28 +0000

    Mon, Mar 11, 2024, 1:38 PM +11:00

--- calculator.py
+++ calculator.py
@@ -1,5 +1,16 @@
-x = int(input("x: "))
+def get_int(prompt):
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            print("Not an integer")
 
-y = int(input("y: "))
+def main():
 
-print(x + y)
+    x= get_int("x: ")
+
+    y = get_int("y: ")
+
+    print(x + y)
+
+main()
