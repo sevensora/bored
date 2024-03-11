b70e94b8c671d6a450a0c599d8c44777bd4895a9
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-11 02:49:24 +0000

    Mon, Mar 11, 2024, 1:49 PM +11:00

--- mario.py
+++ mario.py
@@ -1,4 +1,19 @@
-for i in range(3):
-    for j in range(3):
-        print("#", end="")
-    print()
+
+    do
+
+        x = get_int("Please enter height!: ");
+    }
+    while (x < 1 || x > 8);
+
+    for (int a = 0; a < x; a++)
+    {
+        for (int b = 0; b < x; b++)
+        {
+            if (a + b < x - 1)
+                printf(" ");
+            else
+                printf("#");
+        }
+        printf("\n");
+    }
+}
