ce223b69cae3d62e50413319f8d9902a593558b0
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-11 11:38:08 +0000

    Mon, Mar 11, 2024, 10:38 PM +11:00

--- readability.py
+++ readability.py
@@ -2,8 +2,20 @@ from cs50 import get_string
 
 text = get_string("Text: ")
 
-sentences = letter = words = 0
+sentence = letter = word = 0
 
-for char in text:
-    if char.
+for x in text:
+    if x.isalpha():
+        letters += 1
+    elif x = "":
+        word += 1
+    elif x == "." or x == "?" or i == "!"
+        sentence += 1
+
+index = .0588 * (letter/word * 100) - .296 * (sentence/word * 100) - 15.8
+
+if index < 1:
+    print("Before Grade 1")
+elif index >= 16:
+    print("Grade 16)
 
