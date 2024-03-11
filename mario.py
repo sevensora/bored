d4b9cb8b3a645351c0e52c74b3541fe824cdd1f4
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-11 02:58:26 +0000

    Mon, Mar 11, 2024, 1:58 PM +11:00

--- mario.py
+++ mario.py
@@ -2,27 +2,13 @@ from cs50 import get_int
 
 while True:
     n = get_int('Please enter height!: ')
-    if n > 0:
+    if n > 0 and n < 9:
         break
 
-for i in range(n):
-    print('#')
-
-
-
-        x = get_int("Please enter height!: ");
-    }
-    while (x < 1 || x > 8);
-
-    for (int a = 0; a < x; a++)
-    {
-        for (int b = 0; b < x; b++)
-        {
-            if (a + b < x - 1)
-                printf(" ");
-            else
-                printf("#");
-        }
-        printf("\n");
-    }
-}
+for x in range(0, n , 1):
+    for y in range(0, n , 1):
+        if (x + y < n - 1):
+            print("")
+        else:
+            print('#')
+    print()
