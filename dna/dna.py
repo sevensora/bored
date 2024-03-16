9e0b8a8d0645887a35a2bb962e82865b43d3af2e
sevensora <139036375+sevensora@users.noreply.github.com>
2024-03-16 12:44:07 +0000

    Sat, Mar 16, 2024, 11:44 PM +11:00

--- dna/dna.py
+++ dna/dna.py
@@ -12,8 +12,7 @@ def main():
     # TODO: Read database file into a variable
     with open(sys.argv[1], "r") as data_file:
         reader = csv.DictReader(data_file)
-        for row in reader:
-            database = [row for row in reader]
+        database = [row for row in reader]
 
     # TODO: Read DNA sequence file into a variable
     with open(sys.argv[2], "r") as seq_file:
