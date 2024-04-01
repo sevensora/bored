eced34eefd9bd625e1488f3d90a11f6a9887f8f4
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-01 11:07:30 +0000

    Mon, Apr 1, 2024, 10:07 PM +11:00

--- movies/11.sql
+++ movies/11.sql
@@ -2,5 +2,6 @@ SELECT title FROM people
 JOIN movies ON stars.movie_id = movies.id
 JOIN people ON start.person_id = people.id
 JOIN ratings ON ratings.movie_id = movies.id
-WHERE people.name = "Chadwick Boseman"
+WHERE people.name = "Chadwick Boseman" ORDER BY rating DESC
+LIMIT 5;
 
