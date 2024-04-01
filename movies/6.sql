d7e5489b0cf3546467d8d1643b07b88c53694521
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-01 11:06:00 +0000

    Mon, Apr 1, 2024, 10:06 PM +11:00

--- movies/6.sql
+++ movies/6.sql
@@ -1,2 +1,2 @@
-SELECT AVG(rating) FROM ratings JOIN movies.id = ratings.movie_id
+SELECT AVG(rating) FROM ratings JOIN movies ON movies.id = ratings.movie_id
 WHERE year = 2012;
--- movies/7.sql
+++ movies/7.sql
@@ -1,2 +1,2 @@
 SELECT rating, title FROM movies JOIN ratings ON movie.id = ratings.movie_id WHERE year = 2010
-order by title asc, rating desc;
+ORDER BY title asc, rating desc;
