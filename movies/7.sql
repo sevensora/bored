d4644a14afdd0b8d906bfdc4acce9fd29cdc6429
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-01 10:56:32 +0000

    Mon, Apr 1, 2024, 9:56 PM +11:00

--- movies/7.sql
+++ movies/7.sql
@@ -1,1 +1,2 @@
 SELECT rating, title FROM movies JOIN ratings ON movie.id = ratings.movie_id WHERE year = 2010
+order by title asc;
--- movies/9.sql
+++ movies/9.sql
@@ -1,1 +1,2 @@
 SELECT DISTINCT name FROM people JOIN stars ON people.id = stars.person_id JOIN movies ON movies.id = stars.movie_id WHERE movies.year = 2004
+order by people.birth asc;
