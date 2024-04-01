07952b914076827dcae972651be0e2e06aa556c0
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-01 10:53:25 +0000

    Mon, Apr 1, 2024, 9:53 PM +11:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,6 @@
-SELECT title
-FROM movies
-JOIN
+SELECT title FROM people
+JOIN movies ON stars.movie_id = movies.id
+JOIN people ON start.person_id = people.id
+JOIN ratings ON ratings.movie_id = movies.id
+WHERE people.name = "Chadwick Boseman"
+
