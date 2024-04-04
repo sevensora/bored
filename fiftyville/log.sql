05005e3aad95979b1edf7776fc071692cbb44844
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-04 09:16:16 +0000

    Thu, Apr 4, 2024, 8:16 PM +11:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT description FROM crime_scene_reports WHERE day = 28 AND month = 7;
 
-SELECT transcript FROM interviews WHERE day = 28 AND month = 7 AND transcript like "%bakery%";
+SELECT license_plate, activity, hour, minute FROM bakery_security_logs WHERE hour = 10 AND day = 28 AND month = 7;
