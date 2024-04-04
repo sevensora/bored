514fc3a7a150827c49b07c18a000867bc2308f3a
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-04 09:27:13 +0000

    Thu, Apr 4, 2024, 8:27 PM +11:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -7,4 +7,8 @@ SELECT transcript, name FROM interviews WHERE day = 28 AND month = 7 AND transcr
 
 SELECT license_plate, activity, hour, minute FROM bakery_security_logs WHERE day = 28 AND month = 7 AND hour < 11 and hour > 9;
 
-SELECT account_number, transaction_type, amount
+SELEct amount, account_number, transaction_type FROM atm_transactions WHERE day = 28 AND month = 7 AND atm_location = "Leggett Street";
+
+select people.name from atm_transactions join bank_accounts on bank_accounts.account_number = atm_transactions.acount_number JOIN bakery_security logs ON bakery_security_logs.license_plate = people.license_plate
+JOIN people ON people.id = bank_accounts.person_id Where atm_transactions.day = 28 AND atm_transactions.atm_location = "Leggett Street" AND bakery_security_logs.hour > 9 AND bakery_security_logs.hour < 11
+ORDER BY bakery_security_logs.minute;
