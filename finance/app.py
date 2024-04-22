f8cf0c9812c47c8588021cf0bb12fddd444033b4
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-22 11:05:54 +0000

    Mon, Apr 22, 2024, 9:05 PM +10:00

--- finance/app.py
+++ finance/app.py
@@ -164,5 +164,5 @@ def register():
 def sell():
     if request.method == "GET":
         user_id = session["user_id"]
-        symbols_user = db.execute("SELECT symbol FROM transaction WHERE user_id = :id HAVING SUM(shares), > 0" id=user_id)
+        symbols_user = db.execute("SELECT symbol FROM transaction WHERE user_id = :id HAVING SUM(shares) > 0" id=user_id)
         return render_template("sell.html", symbols = [row["symbol"] for row in rows])
