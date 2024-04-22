03c49bc9fc442c258e73f47c33338d3d89808199
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-22 09:14:20 +0000

    Mon, Apr 22, 2024, 7:14 PM +10:00

--- finance/app.py
+++ finance/app.py
@@ -112,7 +112,10 @@ def quote():
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
-    return apology("TODO")
+    if request.method == "GET":
+        return render_template("register.html")
+    else:
+        
 
 
 @app.route("/sell", methods=["GET", "POST"])
