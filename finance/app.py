dc489f6813020b0bd85e156b7c1fccf598dabeb6
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-22 10:17:07 +0000

    Mon, Apr 22, 2024, 8:17 PM +10:00

--- finance/app.py
+++ finance/app.py
@@ -106,7 +106,15 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+    if request.method == "POST":
+        symbol = request.form.get("symbol")
+        quote = lookup(symbol)
+
+        if not quote:
+            return apology("Symbol not valid", 400)
+        return render_template("quote.html", quote=quote)
+    else:
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
