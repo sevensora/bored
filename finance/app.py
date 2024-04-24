bcbd2c29195bec76c30b0917555dd6615937275d
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-24 11:27:04 +0000

    Wed, Apr 24, 2024, 9:27 PM +10:00

--- finance/app.py
+++ finance/app.py
@@ -50,14 +50,39 @@ def index():
 @login_required
 def buy():
     """Buy shares of stock"""
-    return apology("TODO")
+     if request.method == "POST":
+        shares = request.form.get("shares")
+        symbol = request.form.get("symbol").upper()
+        if not symbol:
+            return apology("Please provide symbol")
+
+        quote = lookup(symbol)
+        if quote is None:
+            return apology("Symbol cannot be found")
+
+        price = quote["price"]
+        total_cost = int(shares)*price
+        cash = db.execute("SELECT cash FROM users WHERE id = :user_id",
+                          user_id=session["user_id"])[0]["cash"]
+        if cash < total_cost:
+            return apology("Looks like you don't have enough bread $$")
+        db.execute("UPDATE users SET cash = cash - :total_cost WHERE id = :user_id",
+                   total_cost=total_cost, user_id=session["user_id"])
+        db.execute("INSERT INTO transactions (user_id, symbol, shares, price) VALUES (:user_id, :symbol, :shares, :price)",
+                   user_id=session["user_id"], symbol=symbol, shares=shares, price=price)
+        flash(f"Bought {shares} shares of {symbol} for {used(total_cost)}!")
+        return redirect("/")
+    else:
+        return render_template("buy.html")
 
 
 @app.route("/history")
 @login_required
 def history():
     """Show history of transactions"""
-    return apology("TODO")
+    transactions = db.execute(
+        "SELECT * FROM transactions WHERE user_id = :user_id ORDER BY timestamp DESC", user_id=session["user_id"])
+    return render_template("history.html", transactions=transactions)
 
 
 @app.route("/login", methods=["GET", "POST"])
