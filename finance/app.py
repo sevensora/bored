5a33c8bcc3e2ec65e895294c4f20b2192629c296
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-24 11:26:00 +0000

    Wed, Apr 24, 2024, 9:26 PM +10:00

--- finance/app.py
+++ finance/app.py
@@ -35,7 +35,15 @@ def after_request(response):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    def index():
+    """Show portfolio of stocks"""
+    user_id = session["user_id"]
+    transactions_db = db.execute(
+        "SELECT symbol, SUM(shares) AS shares, price FROM transactions WHERE user_id = ?", user_id)
+    cash_db = db.execute("SELECT cash FROM users WHERE id = ?", user_id)
+    cash = cash_db[0]["cash"]
+
+    return render_template("index.html", database=transactions_db, cash=cash)
 
 
 @app.route("/buy", methods=["GET", "POST"])
