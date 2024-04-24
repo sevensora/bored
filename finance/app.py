21500fc772e128f28dd9229f20dd5df660442c9a
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-24 10:57:51 +0000

    Wed, Apr 24, 2024, 8:57 PM +10:00

--- finance/app.py
+++ finance/app.py
@@ -167,8 +167,7 @@ def register():
             return apology("Please check password")
         hash = generate_password_hash(password)
         try:
-            new_user = db.execute(
-                "INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
+            new_user = db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", username, hash)
         except:
             return apology("Username already exists")
         session["user_id"] = new_user
