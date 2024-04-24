339da5f9fca2028e3c21fda2307106a737f9e1f8
sevensora <139036375+sevensora@users.noreply.github.com>
2024-04-24 13:29:50 +0000

    Wed, Apr 24, 2024, 11:29 PM +10:00

--- finance/app.py
+++ finance/app.py
@@ -12,7 +12,7 @@ from helpers import apology, login_required, lookup, usd
 app = Flask(__name__)
 
 # Custom filter
-app.jinja_env.filters["usd"] = usd
+app.jinja_env.filters['usd'] = usd
 
 # Configure session to use filesystem (instead of signed cookies)
 app.config["SESSION_PERMANENT"] = False
